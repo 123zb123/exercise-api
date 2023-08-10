9e3f7a0496e49234b022842d0543ec3fb093fede
Zvika Berkovich <zvika0556705015@gmail.com>
2023-08-10 10:04:40 +0300

    finaly exercise Woh

--- learn javaScript/class_exercise7.js
+++ learn javaScript/class_exercise7.js
@@ -1,20 +1,20 @@
 // 1
 
-function successPromise(num) {
-    return new Promise((reslove, reject) => {
-        if (num > 5) reslove("Success")
-        else {
-            reject("Not success")
-        }
-    })
-}
+// function successPromise(num) {
+//     return new Promise((reslove, reject) => {
+//         if (num > 5) reslove("Success")
+//         else {
+//             reject("Not success")
+//         }
+//     })
+// }
 
-successPromise(7)
-.then((message) => {
-    console.log(message)
-}).catch((error) => {
-    console.log(error)
-})
+// successPromise(7)
+// .then((message) => {
+//     console.log(message)
+// }).catch((error) => {
+//     console.log(error)
+// })
 
 //2
 
@@ -33,25 +33,25 @@ function fullName(firstName, lastName) {
 
 //3
 
-function addFive(num){
-    return new Promise((reslove,reject) => {
+function addFive(num) {
+    return new Promise((reslove, reject) => {
         setTimeout(() => {
             reslove(num + 5)
-        },500)
+        }, 500)
     })
 }
-function multiplyByTwo(num){
-    return new Promise((reslove,reject) => {
+function multiplyByTwo(num) {
+    return new Promise((reslove, reject) => {
         setTimeout(() => {
             reslove(num * 2)
-        },500)
+        }, 500)
     })
 }
-function subtractTen(num){
-    return new Promise((reslove,reject) => {
+function subtractTen(num) {
+    return new Promise((reslove, reject) => {
         setTimeout(() => {
             reslove(num - 10)
-        },500)
+        }, 500)
     })
 }
 
@@ -66,21 +66,21 @@ function subtractTen(num){
 
 //4 
 
-function divide(num1, num2){
-    return new Promise((reslove,reject) => {
+function divide(num1, num2) {
+    return new Promise((reslove, reject) => {
         setTimeout(() => {
-            if (num2 === 0){
-            reslove('not divide in 0')
-        }else{
-            reject(num1 / num2)
+            if (num2 === 0) {
+                reslove(num1 / num2)
+            } else {
+                reslove('not divide in 0')
             }
-        },500)
+        }, 500)
     })
 }
 
-// divide(4,0)
-// .then((value) => console.log(value))
-// .catch((error) => console.log(error))
+// divide(4, 0)
+//     .then((value) => console.log(value))
+//     .catch((error) => console.log(error))
 
 
 
@@ -97,11 +97,11 @@ function fullName(firstName, lastName) {
     )
 }
 
-async function main(firstName, lastName){
+async function main(firstName, lastName) {
     const result = await fullName(firstName, lastName)
     console.log(result)
     console.log('hi what\'s up')
-   
+
 }
 
 // main('zvika', 'berkovich')
@@ -111,30 +111,30 @@ async function main(firstName, lastName){
 
 //5 - 3
 
-function addFive(num){
-    return new Promise((reslove,reject) => {
+function addFive(num) {
+    return new Promise((reslove, reject) => {
         setTimeout(() => {
             reslove(num + 5)
-        },500)
+        }, 500)
     })
 }
-function multiplyByTwo(num){
-    return new Promise((reslove,reject) => {
+function multiplyByTwo(num) {
+    return new Promise((reslove, reject) => {
         setTimeout(() => {
             reslove(num * 2)
-        },500)
+        }, 500)
     })
 }
-function subtractTen(num){
-    return new Promise((reslove,reject) => {
+function subtractTen(num) {
+    return new Promise((reslove, reject) => {
         setTimeout(() => {
             reslove(num - 10)
-        },500)
+        }, 500)
     })
 }
 
-async function main(num){
-    let result = await addFive(num)
+async function main(num) {
+    let result = addFive(num)
     let result1 = await multiplyByTwo(result)
     let result3 = await subtractTen(result1)
     console.log(result3)
@@ -143,3 +143,25 @@ async function main(num){
 
 
 //6
+
+function divide(num1, num2) {
+    return new Promise((reslove, reject) => {
+        setTimeout(() => {
+            if (num2 !== 0) {
+                reslove(num1 / num2)
+            } else {
+                reject('not divide in 0')
+            }
+        }, 500)
+    })
+}
+
+async function asyncDivide(num1, num2) {
+    try {
+            await divide(num1, num2)
+        } catch(error) {
+            await divide(num1, num2)
+    }
+}
+
+asyncDivide(1,0).then((value) => console.log(value)).catch((error) => console.error(error))
